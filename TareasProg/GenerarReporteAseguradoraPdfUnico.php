b22b4e8e707ffd3a02b17d3da3beb48a468ad90e
Odalis Dariel Abreu <odalisdabreu@gmail.com>
2024-11-29 19:10:01 -0400

    test

--- TareasProg/GenerarReporteAseguradoraPdfUnico.php
+++ TareasProg/GenerarReporteAseguradoraPdfUnico.php
@@ -501,8 +501,8 @@ sujeto a los términos, límites y condiciones que en ella se expresan y al pago
 
 <tr style="margin-top:-250px">
     <td align="left">
-    <td>
-      <img src="' . $logo . $NombreImg . '"  alt=""/>
+    <td width="100px">
+      <img src="' . $logo . $NombreImg . '"  alt="" width="100px"/>
     </td>
     <td>
       <div align="center" style="color:#6886FD;">
