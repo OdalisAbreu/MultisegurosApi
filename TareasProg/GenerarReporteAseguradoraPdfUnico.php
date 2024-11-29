8f40e8bd8060f7240e277a14cb4c703ab6fd9620
Odalis Dariel Abreu <odalisdabreu@gmail.com>
2024-11-29 18:54:51 -0400

    tets

--- TareasProg/GenerarReporteAseguradoraPdfUnico.php
+++ TareasProg/GenerarReporteAseguradoraPdfUnico.php
@@ -495,7 +495,7 @@ sujeto a los términos, límites y condiciones que en ella se expresan y al pago
 &nbsp;&nbsp;5- Obtenga el nombre y dirección de los lesionados y testigos. <br>
 <br>
 <b>Comuníquese con la aseguradora antes de iniciar cualquier trámite</b><br>
-   <div align="left" style="color:#6886FD; font-size:18px;">
+   <div align="left" style="color:#6886FD; font-size:50px;">
         <br>Asistencia Vial <br>
         809 699 2008
         <br>Casa del Conductor <br>
@@ -504,7 +504,7 @@ sujeto a los términos, límites y condiciones que en ella se expresan y al pago
 </td>
   </tr>
 
-<tr style="margin-top:-50px">
+<tr style="margin-top:-250px">
     <td align="left">
       <img src="' . $logo . $NombreImg . '"  alt="" width="100px"/>
     </td> 
