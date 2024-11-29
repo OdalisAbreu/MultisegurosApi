0f70e872b9b4cb62386493529bf2910830d23cb3
Odalis Dariel Abreu <odalisdabreu@gmail.com>
2024-11-29 17:48:28 -0400

    Fix

--- TareasProg/GenerarReporteAseguradoraPdfUnico.php
+++ TareasProg/GenerarReporteAseguradoraPdfUnico.php
@@ -499,12 +499,14 @@ sujeto a los términos, límites y condiciones que en ella se expresan y al pago
   </tr>
 
 <tr>
+  <td>
     <div align="center" valign="middle" style="color:#6886FD; margin-top:-50px">
       <br>Asistencia Vial <br>
       809 699 2008
       <br>Casa del Conductor <br>
       809 381 2424
     </div>
+  </td>  
     <td colspan="2" align="left">
       <img src="' . $logo . $NombreImg . '"  alt="" width="100px"/>
     </td>
