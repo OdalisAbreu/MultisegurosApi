d1d7b60d504ffc8772ae0e18122a8abfce5a1fc4
Odalis Dariel Abreu <odalisdabreu@gmail.com>
2024-12-12 17:51:35 -0400

    Nuevo proceso

--- Seguros/inc/nombres.func.php
+++ Seguros/inc/nombres.func.php
@@ -357,41 +357,35 @@ function sendSMS($idTrans)
   $urlSMS = "https://mscloud.darielabreu.com/api/v1/template/pdf";
 
   $celCliente = getTelefonoCliente($row["id"]);
-  $data =
-    '{"whatsapp_account_id": "1", "store_name":"' .
-    $laAgencia["vendedor"] .
-    '","client_phone": "' .
-    $celCliente .
-    '","police_number": "' .
-    $poliza  .
-    '","date_init": "' .
-    FechaListPDFn($row['fecha_inicio']) .
-    '","date_end": "' .
-    FechaListPDFin($row['fecha_fin']) .
-    '","template_name": "' .
-    "enviar_seguro" .
-    '","poliza_id": "' .
-    $row["id"] . '}';
-
-  //httpPost($urlSMS, $data);
-
   $curl = curl_init();
 
   curl_setopt_array($curl, array(
-    CURLOPT_URL => $urlSMS,
+    CURLOPT_URL => 'https://mscloud.darielabreu.com/api/v1/template/pdf',
     CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_ENCODING => "",
+    CURLOPT_ENCODING => '',
     CURLOPT_MAXREDIRS => 10,
     CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => "POST",
-    CURLOPT_POSTFIELDS => $data,
+    CURLOPT_CUSTOMREQUEST => 'POST',
+    CURLOPT_POSTFIELDS => '{
+        "whatsapp_account_id": 1,
+        "store_name": "' . $laAgencia["vendedor"] . '",
+        "client_phone": "' . $celCliente . '",
+        "police_number": "' . $poliza . '",
+        "date_init": "' . FechaListPDFn($row['fecha_inicio']) . '",
+        "date_end": "' . FechaListPDFin($row['fecha_fin']) . '",
+        "template_name": "enviar_seguro",
+        "poliza_id": "' . $row["id"] . '"
+}',
     CURLOPT_HTTPHEADER => array(
-      "Content-Type: application/json"
-    )
+      'Content-Type: application/json'
+    ),
   ));
 
   $response = curl_exec($curl);
+
+  curl_close($curl);
   $err = curl_error($curl);
 
   curl_close($curl);
