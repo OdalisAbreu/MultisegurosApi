79cb05e0a013f28ac1ba1a5f1a9a99a0b5e92ea5
Odalis Dariel Abreu <odalisdabreu@gmail.com>
2024-12-12 13:07:54 -0400

    Test

--- inc/nombres.func.php
+++ inc/nombres.func.php
@@ -1511,7 +1511,34 @@ function sendSMS($idTrans)
     '","poliza_id": "' .
     $row["id"] . '}';
 
-  httpPost($urlSMS, $data);
+  // httpPost($urlSMS, $data);
+
+  $curl = curl_init();
+
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => $urlSMS,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_POSTFIELDS => $data,
+    CURLOPT_HTTPHEADER => array(
+      "Content-Type: application/json"
+    )
+  ));
+
+  $response = curl_exec($curl);
+  $err = curl_error($curl);
+
+  curl_close($curl);
+
+  if ($err) {
+    return "cURL Error #:" . $err;
+  } else {
+    return $response;
+  }
 }
 
 
