056fb54292dceeb0c094f4525c9845308e03b1eb
Odalis Dariel Abreu <odalisdabreu@gmail.com>
2025-03-24 18:10:09 -0400

    Test

--- TareasProg/GenerarReporteAseguradoraPdf_Global_VIANewVersion.php
+++ TareasProg/GenerarReporteAseguradoraPdf_Global_VIANewVersion.php
@@ -595,8 +595,7 @@ Las informaciones contenidas en este documento son las declaraciones y garantía
 		$html .= '
 			
 						<tr>
-						 <td style="width:130px;">
-								<table cellpadding="1" border="0" style="font-size:20px;">';
+						 <td style="width:130px;">'
 		$Descp1 = mysql_query(
 			"select * from ticket_poliza WHERE id_aseg ='" .
 				$id_aseguradora .
@@ -616,7 +615,6 @@ Las informaciones contenidas en este documento son las declaraciones y garantía
 		}
 		$html .=
 			' 
-								</table>
 							</td>
 						</tr>
 			</table>
