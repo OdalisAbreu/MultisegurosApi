dac02d409be45437d50cd795e5b80de48ec529d9
Odalis Dariel Abreu <odalisdabreu@gmail.com>
2025-03-25 19:28:44 -0400

    Test

--- TareasProg/GenerarReporteAseguradoraPdf_Global_VIANewVersion.php
+++ TareasProg/GenerarReporteAseguradoraPdf_Global_VIANewVersion.php
@@ -605,10 +605,10 @@ sujeto a los términos, límites y condiciones que en ella se expresan y al pago
 <tr cellspacing="0">
     <td align="left">
     <td width="100px">
-      <img src="' . $logo . $NombreImg . '"  alt="" width="100px"/>
+      <img src="' . $logo . $NombreImg . '"  alt="" width="100px"/> 
     </td>
     ';
-		$html_servicios_opcionales;
+		print($RowHist2['tipo']);
 		$QueryH5 = mysql_query(
 			"select * from seguro_trans_history   
 	WHERE id_trans ='" .
