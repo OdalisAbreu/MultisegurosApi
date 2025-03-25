e0c9b7c9df8e12d344f7560e2e2d57c64c69fcbe
Odalis Dariel Abreu <odalisdabreu@gmail.com>
2025-03-25 19:41:59 -0400

    Test

--- TareasProg/GenerarReporteAseguradoraPdf_Global_VIANewVersion.php
+++ TareasProg/GenerarReporteAseguradoraPdf_Global_VIANewVersion.php
@@ -399,27 +399,6 @@ Las informaciones contenidas en este documento son las declaraciones y garantía
 			} else {
 				$montoSeguro = $RowHist['monto'];
 			}
-
-			if ($RowHist['tipo'] == 'serv') {
-				$html_servicios_opcionales .= ' <td>
-      <div align="center" style="color:#6886FD;">';
-				if (ServAdicHistory($RowHist['id_serv_adc']) == "Asistencia Vial (Grua)") {
-					$html_servicios_opcionales .=
-						'
-         <br>Asistencia Vial (Grua)<br>
-          809 699 2008';
-				}
-
-				if (ServAdicHistory($RowHist['id_serv_adc']) == "Casa del Conductor") {
-					$html_servicios_opcionales .=
-						'
-          <br>Casa del Conductor <br>
-          809 381 2424';
-				}
-				$html_servicios_opcionales .= '
-      </div>    
-    </td>';
-			}
 		}
 
 		$html .=
@@ -608,31 +587,25 @@ sujeto a los términos, límites y condiciones que en ella se expresan y al pago
       <img src="' . $logo . $NombreImg . '"  alt="" width="100px"/> 
     </td>
     ';
-		$QueryH5 = mysql_query(
-			"select * from seguro_trans_history   
-	WHERE id_trans ='" .
-				$row['id'] .
-				"'"
-		);
 
-		while ($RowHist2 = mysql_fetch_array($QueryH5)) {
+		while ($RowHist2 = mysql_fetch_array($QueryH)) {
 			if ($RowHist2['tipo'] == 'serv') {
-				$html .= ' <td>
+				$html_servicios_opcionales .= ' <td>
       <div align="center" style="color:#6886FD;">';
-				if (ServAdicHistory($RowHist2['id_serv_adc']) == "Asistencia Vial") {
-					$html .=
+				if (ServAdicHistory($RowHist2['id_serv_adc']) == "Asistencia Vial (Grua)") {
+					$html_servicios_opcionales .=
 						'
          <br>Asistencia Vial (Grua)<br>
           809 699 2008';
 				}
 
 				if (ServAdicHistory($RowHist2['id_serv_adc']) == "Casa del Conductor") {
-					$html .=
+					$html_servicios_opcionales .=
 						'
           <br>Casa del Conductor <br>
           809 381 2424';
 				}
-				$html .= '
+				$html_servicios_opcionales .= '
       </div>    
     </td>';
 			}
