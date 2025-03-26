72906e73194701216363621c727b7c1b316c18ac
Odalis Dariel Abreu <odalisdabreu@gmail.com>
2025-03-26 18:17:33 -0400

    Test

--- TareasProg/GenerarReporteAseguradoraPdfUnico.php
+++ TareasProg/GenerarReporteAseguradoraPdfUnico.php
@@ -493,13 +493,15 @@ sujeto a los términos, límites y condiciones que en ella se expresan y al pago
 &nbsp;&nbsp;4- Obtenga el número de placa, aseguradora, y número de póliza. <br>
 &nbsp;&nbsp;5- Obtenga el nombre y dirección de los lesionados y testigos. <br>
 <br>
-<b>Comuníquese con la aseguradora antes de iniciar cualquier trámite:</b>
+<b>Comuníquese con la aseguradora antes de iniciar cualquier trámite</b>
+
 </td>
   </tr>
+
 <tr cellspacing="0">
     <td align="left">
-    <td width="80px">
-      <img src="' . $logo . $NombreImg . '"  alt="" width="80px"/>
+    <td width="100px">
+      <img src="' . $logo . $NombreImg . '"  alt="" width="100px"/>
     </td>
     ';
 $QueryH5 = mysql_query(
--- TareasProg/GenerarReporteAseguradoraPdf_Global_VIANewVersion.php
+++ TareasProg/GenerarReporteAseguradoraPdf_Global_VIANewVersion.php
@@ -589,7 +589,6 @@ Las informaciones contenidas en este documento son las declaraciones y garantía
     <td colspan="2" style="font-size:18px" align="left">
 El vehículo descrito en el anverso está asegurado bajo la póliza emitida por La Aseguradora, <br>
 sujeto a los términos, límites y condiciones que en ella se expresan y al pago de la prima. <br>
-<br>
 <b>En caso de accidente:</b> <br>
 &nbsp;&nbsp;1- Asista a los lesionados, si los hubiere. Con cuidado, retire los vehículos de la vía. <br>
 &nbsp;&nbsp;2- No acepte responsabilidad al momento del accidente; reserve su derecho. <br>
@@ -597,15 +596,15 @@ sujeto a los términos, límites y condiciones que en ella se expresan y al pago
 &nbsp;&nbsp;4- Obtenga el número de placa, aseguradora, y número de póliza. <br>
 &nbsp;&nbsp;5- Obtenga el nombre y dirección de los lesionados y testigos. <br>
 <br>
-<b>Comuníquese con la aseguradora antes de iniciar cualquier trámite</b><br>
+<b>Comuníquese con la aseguradora antes de iniciar cualquier trámite</b>
 </td>
   </tr>
 		</table>
 		
 <tr cellspacing="0">
     <td align="left">
-    <td width="100px">
-      <img src="' . $logo . $NombreImg . '"  alt="" width="100px"/> 
+    <td width="80px">
+      <img src="' . $logo . $NombreImg . '"  alt="" width="80px"/> 
     </td>
 	</td> 
     ';
@@ -614,9 +613,7 @@ sujeto a los términos, límites y condiciones que en ella se expresan y al pago
 		}
 		$html .= '
 </tr>
-
-
-			    <tr>
+		    <tr>
 					<td style="width:130px;">
 			  <table cellpadding="1" border="0" style="font-size:20px; min-height:80px;">';
 		$Descp1 = mysql_query(
